d71213a72231807e642c1f221441a629025ce42c
Oscar Andres Ramos Lopez <oscar1940_1@hotmail.com>
2023-01-06 16:30:09 -0500

    mejora funcional con operadores ternarios

--- src/work/oscarramos/tarea/modificador/TareaFizzBuzz.java
+++ src/work/oscarramos/tarea/modificador/TareaFizzBuzz.java
@@ -1,6 +1,7 @@
 package work.oscarramos.tarea.modificador;
 
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 //Escriba un programa que imprima los números del 1 al 100 y para múltiplos de '3' imprima "Fizz" en lugar del número y para los múltiplos de '5' imprima "Buzz".
@@ -8,28 +9,8 @@ import java.util.stream.Stream;
 
 public class TareaFizzBuzz {
     public static void main(String[] args) {
-        AtomicInteger numeros = new AtomicInteger(0);
-        Stream<String> resultado =   Stream.generate(numeros::incrementAndGet)
-                .limit(100)
-                .flatMap(d-> {
-                    String cadena = " ";
-                    if(d % 3 == 0){
-                        cadena="Fizz";
-                    }
-                    if(d % 5 ==0){
-                        if(!cadena.isBlank()){
-                            cadena= cadena + "Buzz";
-                        }else{
-                            cadena= "Buzz";
-                        }
-                    }
-                    if(cadena.isBlank()){
-                        cadena = d.toString();
-                    }
-                     return Stream.of(cadena);
-                });
-
-
-        resultado.forEach(System.out::println);
+        IntStream.rangeClosed(1,100)
+                        .mapToObj(i->i % 3 == 0 ?(i % 5 == 0 ? "FizzBuzz":"Fizz"): (i % 5 == 0 ? "Buzz" : i))
+                                .forEach(System.out::println);
     }
 }
