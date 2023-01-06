551463655164bd7cd4af92661b1996aab5f7a50f
Oscar Andres Ramos Lopez <oscar1940_1@hotmail.com>
2023-01-06 16:36:33 -0500

    identacion

--- src/work/oscarramos/tarea/modificador/TareaFizzBuzz.java
+++ src/work/oscarramos/tarea/modificador/TareaFizzBuzz.java
@@ -9,8 +9,8 @@ import java.util.stream.Stream;
 
 public class TareaFizzBuzz {
     public static void main(String[] args) {
-        IntStream.rangeClosed(1,100)
-                        .mapToObj(i->i % 3 == 0 ?(i % 5 == 0 ? "FizzBuzz":"Fizz"): (i % 5 == 0 ? "Buzz" : i))
-                                .forEach(System.out::println);
+        IntStream.rangeClosed(1, 100)
+                .mapToObj(i -> i % 3 == 0 ? (i % 5 == 0 ? "FizzBuzz" : "Fizz") : (i % 5 == 0 ? "Buzz" : i))
+                .forEach(System.out::println);
     }
 }
